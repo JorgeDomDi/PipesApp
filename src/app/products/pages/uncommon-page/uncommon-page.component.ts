cec9692430fd8ae073a3858eac27db4f3d29a2f1
Jorge Martin Dominguez Diaz <jorgemartindominguezdiaz@gmail.com>
2024-10-13 22:43:39 -0300

    slice pipe

--- src/app/products/pages/uncommon-page/uncommon-page.component.ts
+++ src/app/products/pages/uncommon-page/uncommon-page.component.ts
@@ -7,6 +7,7 @@ import { Component } from '@angular/core';
 })
 export class UncommonPageComponent {
 
+
 //i18nSelect
 public name:string='Fernando'
 public gender:'Female'|'Male'='Male'
@@ -14,8 +15,19 @@ public invitationMap={
   Male:'invitarlo',
   Female:'invitarla',
 }
+public clientsMap={
+  '=0':'Actualmente no tenemos ningún cliente esperando',
+  '=1':'Actualmente tenemos un cliente esperando',
+  'other':'Actualmente tenemos # clientes esperando.'
+ 
+}
+//i18nPlural
+public clients:string[]=["Pedro","Maria","Fenando","Jorge","Nalatlia"]
 changeClient() {
   this.name="Melisa",
   this.gender="Female"
   }
+  borrarCliente() {
+ this.clients.shift()
+    }
 }
