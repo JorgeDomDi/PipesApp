405b20372699e163933a57f06e8ba51b20668b0b
Jorge Martin Dominguez Diaz <jorgemartindominguezdiaz@gmail.com>
2024-10-13 23:04:00 -0300

    finalizando seccion Pipes PrimeNg

--- src/app/products/pages/uncommon-page/uncommon-page.component.ts
+++ src/app/products/pages/uncommon-page/uncommon-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { interval, tap } from 'rxjs';
 
 @Component({
   selector: 'app-uncommon-page',
@@ -30,4 +31,20 @@ changeClient() {
   borrarCliente() {
  this.clients.shift()
     }
+    //keyValue pipe
+    public person={
+      name:"Fernando",
+      age:36,
+      address:"Ottawa, Canada",
+    }
+    //async pip
+    public myObservableTimer=interval(2000).pipe(
+      tap(value=>console.log('tap: ',value)
+      )
+    );
+    public promiseValue:Promise<string>=new Promise((resolve,reject)=>{
+    setTimeout(()=>{
+      resolve('Tenemos data en la promesa')
+    },3500)
+    })
 }
